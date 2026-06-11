bd60a9aa3393b75e1cd724460f508d3191a72973
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:36:12 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for Jk Bhasin 
 
 Firm facts:
 - Firm: Jk Bhasin Reealty
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: Booth No. 29, Sector 20 Market Complex, Panchkula, Haryana 134116
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
